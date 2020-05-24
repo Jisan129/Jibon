d74b507faf8bbfad8e401a7b908f4d45b282f40b
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2020-05-24 15:58:12 +0600

    login xml

--- app/src/main/java/com/example/jibon/login_register/login_activity.java
+++ app/src/main/java/com/example/jibon/login_register/login_activity.java
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.jibon.R;
 
 public class login_activity extends AppCompatActivity {
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
+        signUpButton = findViewById(R.id.button_signup);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToRegisterActivity();
+            }
+        });
     }
 
-    private void SendUserToRegisterActivity() {
+    public void SendUserToRegisterActivity() {
         Intent registerIntent = new Intent(login_activity.this,register_activity.class);//going from login activity to main activity
         startActivity (registerIntent);
 
